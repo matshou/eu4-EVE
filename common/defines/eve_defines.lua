9191a81477e583f903b7ad7e425e6359940a9922
Matthew Cain <yooksi@zoho.com>
2020-01-08 12:58:53 +0100

    :heavy_plus_sign: Add define override file
    For increased compatibility when making a release.

---
+++ common/defines/eve_defines.lua
@@ -0,0 +1,20 @@
+-- Put override defines here:
+--------------------------------------------------------
+-------------------- Estate defines --------------------
+--------------------------------------------------------
+NDefines.NCountry.ESTATE_PROVINCE_HAPPINESS_INCREASE = 1.0
+NDefines.NCountry.ESTATE_GRANT_PROVINCE_COOLDOWN = 1
+--------------------------------------------------------
+---------------- Innovativeness defines ----------------
+--------------------------------------------------------
+NDefines.NCountry.INNOVATIVENESS_BEHIND_IN_TECH_PENALTY = -0.03
+NDefines.NCountry.INNOVATIVENESS_ENABLE_ANY_BEHIND_IN_TECH_PENALTY = 0
+NDefines.NCountry.INNOVATIVENESS_ENABLE_PER_TECH_BEHIND_IN_TECH_PENALTY = 0
+NDefines.NCountry.INNOVATIVENESS_AHEAD_OF_TIME_BONUS = 0.005
+NDefines.NCountry.INNOVATIVENESS_DAYS_AFTER_FIRST_PICK_VIABLE = 90
+--------------------------------------------------------
+------------------- Military defines -------------------
+--------------------------------------------------------
+NDefines.NMilitary.ARMY_DRILL_YEARLY_DECAY = -2.5
+NDefines.NMilitary.ARMY_DRILL_YEARLY_GAIN = 10.0
+NDefines.NMilitary.ARMY_DRILL_SKILL_MONTHS = 120
